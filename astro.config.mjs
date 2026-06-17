11f7cb6cad54b987fcbd7b541831207a36ca4a4b
La Alquimia <laalquimia@Soleimani.local>
2026-06-16 23:31:13 -0500

    Hackaton 🐢

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://LaAlquimia.github.io',
-  base: '/Hackaton',
+  base: '/hackaton',
   output: 'static',
 });
